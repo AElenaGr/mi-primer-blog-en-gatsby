9dbc54ba21aa0d1ad173f3a082d2508eddcb3f3f
AElenaGr <andrae.tandemaranjuez@gmail.com>
2024-03-22 09:19:44 +0100

    parte 3: añadir nuevas funcionalidades mediante plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,19 @@
 import * as React from "react";
 //import {Link} from 'gatsby' // {} import algo dentro de algo
 
-
 import Layout from "../componentes/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // Step 2: Define your component. LO QUE HAY DENTRO DEL LAYOUT ES EL CHILDREN
 const IndexPage = () => {
   return (
-    <Layout pageTitle='Pagina de inicio'> 
-      <p>Esta es la pagina de inicio</p> 
+    <Layout pageTitle="Pagina de inicio">
+      <p>Esta es la pagina de inicio</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      <StaticImage src="../images/chinescos.jpg"></StaticImage>
     </Layout>
 
     // <main>
